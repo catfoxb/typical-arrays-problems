031898d207fe5097149e6e4d85d9d4f8815eeffa
catfoxb <catfoxb@gmail.com>
2021-03-10 09:44:56 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-
-exports.min = function min (array) {
-  return 0;
+function validateArray(array) {
+    if (!Array.isArray(array) || !array.length) return true;
 }
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.min = function min(array) {
+    if (validateArray(array)) return 0;
+    return Math.min(...array);
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (validateArray(array)) return 0;
+    return Math.max(...array)
+};
+
+exports.avg = function avg(array) {
+    if (validateArray(array)) return 0;
+    return array.reduce((item, accum) => {
+        return item + accum;
+    }, 0) / array.length;
+};
